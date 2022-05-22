6a1db19d607eeeb1a2ce02a7e7a3cf9aae74195a
Erik Nyquist <eknyquist@gmail.com>
2022-05-21 20:01:50 -0700

    Update comment headers

--- app_timer.c
+++ app_timer.c
@@ -20,8 +20,8 @@
  *
  *        3. Ensure that either APP_TIMER_COUNT_UINT8, APP_TIMER_COUNT_UINT16, or
  *           APP_TIMER_COUNT_UINT32 is set -- pick one that is large enough to hold
- *           all the bits of your hardware counter. For example, if you had a 12-bit
- *           counter, you could use either APP_TIMER_COUNT_UINT16 or APP_TIMER_COUNT_UINT32.
+ *           all the bits of your hardware counter. For example, if you had a 24-bit
+ *           counter, you could use APP_TIMER_COUNT_UINT32, but not APP_TIMER_COUNT_UINT16.
  *           If you don't define one of these options, the default is APP_TIMER_COUNT_UINT32.
  *
  *        4. Call app_timer_init() and pass in a pointer to the HW model you created
